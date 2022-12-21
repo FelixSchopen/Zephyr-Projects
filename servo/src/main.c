de0c8181b4b7321747b933ccfb79505c823e0484
felix <felixschopen@googlemail.com>
2022-12-21 15:07:49 +0100

    servo

--- servo/src/main.c
+++ servo/src/main.c
@@ -9,15 +9,17 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/pwm.h>
 #include <zephyr/devicetree.h>
-#include <zephyr/sys/printk.h>
+
+const struct device* pwm_dev = DEVICE_DT_GET(DT_NODELABEL(pwm1));
 
 void main(void){
 
-    const struct device* pwm = device_get_binding("PWM");
 
-    pwm_set(pwm, 2, PWM_MSEC(600), PWM_SEC(300), PWM_POLARITY_NORMAL);
 
     while(1){
-
+        pwm_set(pwm_dev, 2, PWM_MSEC(20), PWM_MSEC(0.5), PWM_POLARITY_NORMAL);
+        k_msleep(2000);
+        pwm_set(pwm_dev, 2, PWM_MSEC(20), PWM_MSEC(2.5), PWM_POLARITY_NORMAL);
+        k_msleep(2000);
     }
 }
