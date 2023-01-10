2b5288a797f619141066c1f4a7ff5c310a64dcf9
felix <felixschopen@googlemail.com>
2023-01-10 12:52:47 +0100

    can loopback working

--- can/src/main.c
+++ can/src/main.c
@@ -10,20 +10,25 @@
 #include <zephyr/drivers/pwm.h>
 #include <zephyr/drivers/can.h>
 #include <zephyr/devicetree.h>
+#include <stdio.h>
 #include "SEGGER_RTT.h"
+
+CAN_MSGQ_DEFINE(counter_msgq, 2);
+
 
 const struct device* can1_dev = DEVICE_DT_GET(DT_NODELABEL(can1));
 
 void t_sender(void){
     while(1){
-        const struct can_frame frame = {
-            .flags = CAN_FRAME_IDE,
+        struct can_frame frame = {
             .id = 0x123, 
             .dlc = 1, 
-            .data = {10}
+            .flags = CAN_FRAME_IDE
         };
 
-        if(can_send(can1_dev, &frame, K_MSEC(100), NULL, NULL) != 0){
+        frame.data[0] = 10;
+
+        if(can_send(can1_dev, &frame, K_MSEC(100), NULL, "Sender Task") != 0){
             SEGGER_RTT_WriteString(0, "Failed to send can_frame!\n");
         }
         else{
@@ -35,40 +40,35 @@ void t_sender(void){
 }
 
 void can_receiver_callback(const struct device *dev, struct can_frame *frame, void *user_data){
-
-    //uint8_t* data = (*uint8_t)user_data;
+    uint8_t data = frame->data[0];
     SEGGER_RTT_WriteString(0, "Received can_frame\n");
-}
-
-void t_receiver(void){
-
-    void* data;
-
-    const struct can_filter filter = {
-        .id = 0x123,
-        .mask = 0xFF,
-        .res0 = 0xFF,
-        .flags = CAN_FILTER_IDE
-    };
-
-    while(1){
-
-        if(can_add_rx_filter(can1_dev, (can_rx_callback_t)can_receiver_callback, data, &filter) == filter.id){
-            SEGGER_RTT_WriteString(0, "Added can_filter\n");
-        }
-        else{
-            SEGGER_RTT_WriteString(0, "Can not add can_filter\n\n");
-        }
-
-        k_msleep(1000);
+    if(data == 10){
+        SEGGER_RTT_WriteString(0, "Data: 10\n");
+        printf("Test\n");
+    }
+    if(data != 10){
+        SEGGER_RTT_WriteString(0, "Error\n");
     }
 }
 
 K_THREAD_DEFINE(sender, 1024, t_sender, NULL, NULL, NULL, -5, 0, 0);
-K_THREAD_DEFINE(receiver, 1024, t_receiver, NULL, NULL, NULL, -5, 0, 0);
-
 
 void main(void){ 
     can_set_mode(can1_dev, CAN_MODE_LOOPBACK);
     can_start(can1_dev);
+
+    const struct can_filter my_filter = {
+		.id = 0x123,
+        .flags = CAN_FILTER_IDE | CAN_EXT_ID_MASK
+	};
+
+    int filter_id = can_add_rx_filter(can1_dev, can_receiver_callback, NULL, &my_filter);
+
+    if(filter_id < 0){
+        SEGGER_RTT_WriteString(0, "Error while adding filter!\n");
+    }
+    else {
+        SEGGER_RTT_WriteString(0, "Filter added!\n");
+    }
+  
 }
